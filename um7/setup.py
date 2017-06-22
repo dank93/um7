94644208b9ee97b15a894bad74a4a38a388294c6
dank93 <dkurek93@gmail.com>
2017-06-22 17:34:44 -0400

    more update

--- um7/setup.py
+++ um7/setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(name='um7',
-      version='0.11',
+      version='0.12',
       description='Classes to interface with CH Robotics UM7 IMU',
-      url='https://pypi.python.org/pypi/um7/0.11',
+      url='https://pypi.python.org/pypi/um7/0.12',
       author='Daniel Kurek',
       author_email='dkurek93@gmail.com',
       license='MIT',
